1d2a826358b2a2f8e99a485a2938d5129118937f
Kareltheketa <102698582+Kareltheketa@users.noreply.github.com>
2024-09-28 11:52:52 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Banner from "components/ui/Banner/Banner";
 import CTA from "components/ui/CTA";
 import Features from "components/ui/Features/Features";
 import Hero from "components/ui/Hero";
-import SupportedFrameworks from "components/ui/SupportedFrameworks";
-import TemplateCTA from "components/ui/TemplateCTA";
 
 export default () => {
   return (
