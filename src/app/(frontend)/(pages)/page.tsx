8781d4c9eb1525450891bb5f25ec222d5051248d
Kareltheketa <102698582+Kareltheketa@users.noreply.github.com>
2024-10-03 09:51:41 +0200

    website

--- src/app/(frontend)/(pages)/page.tsx
+++
@@ -1,5 +0,0 @@
-import PageTemplate, { generateMetadata } from './[...slug]/page.js'
-
-export default PageTemplate
-
-export { generateMetadata }
--- src/utilities/is-expanded-doc.ts
+++
@@ -1,4 +0,0 @@
-export function isExpandedDoc<T>(doc: T | string): doc is T {
-  if (typeof doc === 'object' && doc !== null) return true
-  return false
-}
