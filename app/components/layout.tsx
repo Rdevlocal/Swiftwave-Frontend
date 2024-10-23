8e1f1c83d18240b86effff575f25c039a6ac7b8c
Kareltheketa <102698582+Kareltheketa@users.noreply.github.com>
2024-10-23 09:35:23 +0200

    seo

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,4 +1,3 @@
-import Sidebar from "components/ui/Sidebar";
 import { ReactNode } from "react";
 
 const title = "Modern UI Components | Float UI";
@@ -24,7 +23,6 @@ export default (props: Props) => {
   return (
     <main className="relative custom-screen-lg">
       <div className="lg:flex">
-        <Sidebar />
         <div className="flex-1 overflow-hidden mt-20 mb-12 xl:px-8 xl:mt-10">
           {children}
         </div>
