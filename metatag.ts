6066e44fe41499cfba892436e172abf08963068f
Kareltheketa <102698582+Kareltheketa@users.noreply.github.com>
2024-10-23 09:17:08 +0200

    aanpassingen

--- metatag.ts
+++ metatag.ts
@@ -1,5 +1,4 @@
 export default {
-  title: "Float UI - Free open source Tailwind UI components & Templates",
-  desc: "Beautiful and responsive website templates and UI components for React, Vue, Svelte, HTML with Tailwind CSS.",
-  ogImage: "/thumbnail.png?v2",
+  title: "Swiftwave.io",
+  desc: "A plug and play data company.",
 };
